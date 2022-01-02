9c571d8ea17da40c54a6ee0540c3ca0b3d1d80e2
Or Yaacov <oryaacov@live.com>
2022-01-02 08:05:40 +0200

    fix(article): serilaize json as camel case

--- backend/internal/models/article.go
+++ backend/internal/models/article.go
@@ -1,11 +1,11 @@
 package models
 
 type Article struct {
-	Id          string `bson:"_id"`
-	Href        string `bson:"href"`
-	Title       string `bson:"title"`
-	Image       string `bson:"image"`
-	Summary     string `bson:"summary"`
-	PublishedAt uint   `bson:"publishDate"`
-	Body        string `bson:"body,omitempty"`
+	Id          string `json:"id" bson:"_id"`
+	Href        string `json:"sumhrefmary" bson:"href"`
+	Title       string `json:"title" bson:"title"`
+	Image       string `json:"img" bson:"image"`
+	Summary     string `json:"summary" bson:"summary"`
+	PublishedAt uint   `json:"date" bson:"publishDate"`
+	Body        string `json:"body" bson:"body,omitempty"`
 }
