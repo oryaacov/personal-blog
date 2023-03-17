ba29509cc9085c5e7cf05c2d2ade746c8214b68a
Or Yaacov <oryaacov@live.com>
2023-03-17 20:04:33 +0200

    fix(google analytics): switch to html script tag

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,11 +6,9 @@ import Data from '../../public/data.json';
 import Navbar from '../navbar/navbar';
 import Profile from '../profile/profile';
 import './app.css';
-import ReactGA from 'react-ga';
 
 const initialize = () => {
-  ReactGA.initialize('UA-000000-01');
-  ReactGA.pageview(window.location.pathname + window.location.search);
+
 }
 
 const App = () => {
